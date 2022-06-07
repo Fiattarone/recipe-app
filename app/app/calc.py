d6979da25f11ab82bfca41a4a385d0136f45a9dd
Fiattarone <Fiattarone@me.com>
2022-06-06 22:46:29 -0700

    Install postgres db adapter.

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+""" 
+Calculator Functions
+"""
+
+
+def add(x, y):
+    """Returns sum of two numbers"""
+    return x + y
+
+
+def sub(x, y):
+    """Returns difference of two numbers"""
+    return x - y
